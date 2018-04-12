fa2f79c98ad3ca13960c9e9753445442a60ed513
Tomohiro_Ueda <ueda_t@sc0ville.com>
2018-04-12 13:03:39 +0900

    make contact and root

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
   #最初に動く
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
@@ -23,5 +27,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
