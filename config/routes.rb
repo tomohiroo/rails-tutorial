a664f6339b2b3bae53a0561c81ba767403e85dd5
Tomohiro_Ueda <ueda_t@sc0ville.com>
2018-04-15 15:42:45 +0900

    check

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  resources :users
+
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
