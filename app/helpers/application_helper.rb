d929bbf0b8b4bb8ce74de160e033e9dc950934a7
Tomohiro_Ueda <ueda_t@sc0ville.com>
2018-08-15 01:36:54 +0900

    add user following

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def full_title(page_title = "")
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
-      return base_title #returnなしでもok
+      base_title
     else
       return page_title + " | " + base_title
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                foreign_key: "follower_id",
+                                dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                foreign_key: "followed_id",
+                                dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save { email.downcase! }
@@ -65,9 +73,37 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+   following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+   Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
+
+
+    # Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+    #  following_ids: following_ids, user_id: id)
+    # でもいい
+
+    # SELECT "microposts".* FROM "microposts" WHERE (user_id IN
+    #  (4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+    #   41,42,43,44,45,46,47,48,49,50,51)
+    #   OR user_id = 1) ORDER BY "microposts"."created_at" DESC
+    # が発行される
   end
-  
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
     # 有効化トークンとダイジェストを作成および代入する
